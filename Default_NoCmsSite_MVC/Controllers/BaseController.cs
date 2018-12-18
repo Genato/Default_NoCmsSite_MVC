72b0d7c8b608f596531febd84c78f7c306d630ad
Renato.Katalenic <Renato.Katalenic@outlook.com>
2018-12-18 21:24:20 +0100

    Add project files.

---
+++ Default_NoCmsSite_MVC/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Default_NoCmsSite_MVC.Controllers
+{
+  /// <summary>
+  /// Bazni kontroler kojeg nasljeđuju svi kontroleri ove aplikacije
+  /// </summary>
+  public class BaseController : Controller
+  {
+    /// <summary>
+    /// Exception handling.
+    /// </summary>
+    /// <param name="filterContext"></param>
+    protected override void OnException(ExceptionContext filterContext)
+    {
+      base.OnException(filterContext);
+    }
+  }
+}
---
+++ Default_NoCmsSite_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Default_NoCmsSite_MVC.MvcApplication" Language="C#" %>
